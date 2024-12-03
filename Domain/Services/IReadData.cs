620d65514bf2af9989bb0b535e9294a67ea4b1ae
Danijel Jovanović <danijel.xda@gmail.com>
2024-12-03 10:11:54 +0100

    prepare read and write

---
+++ Domain/Services/IReadData.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public interface IReadData
+    {
+        public IEnumerable<Merenje> ProcitajSvaMerenjaPoDeviceId(int deviceId);
+
+        public Merenje ProcitajNajnovijeMerenjePoDeviceId(int deviceId);
+
+        public IEnumerable<Merenje> ProcitajNajnovijeMerenjeZaSvakiDevice();
+
+        public IEnumerable<Merenje> ProcitajMerenjaPoTipu(TipMerenja tip);
+    }
+}
---
+++ Services/ServerReadDataServices/ServerReadDataService.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using Domain.Repositories.MerenjaRepositories;
+using Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ServerReadDataServices
+{
+    public class ServerReadDataService : IReadData
+    {
+        IMerenjaRepository repo = new MerenjaRepository();
+
+        public IEnumerable<Merenje> ProcitajMerenjaPoTipu(TipMerenja tip)
+        {
+            List<Merenje> pronadjena = [];
+
+            foreach(Merenje m in repo.SvaMerenja())
+            {
+                if(m.Tip == tip)
+                    pronadjena.Add(m);
+            }
+
+            return pronadjena;
+        }
+
+        public Merenje ProcitajNajnovijeMerenjePoDeviceId(int deviceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Merenje> ProcitajNajnovijeMerenjeZaSvakiDevice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Merenje> ProcitajSvaMerenjaPoDeviceId(int deviceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
