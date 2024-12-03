802281c3c16368c6a14bd7f9f70662e91792f689
Danijel Jovanović <danijel.xda@gmail.com>
2024-12-03 09:51:37 +0100

    prepare device

---
+++ Domain/Models/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Device
+    {
+        public int Id { get; set; } = 0;
+
+        public string Naziv {  get; set; } = string.Empty;
+
+        public Device() { }
+
+        public Device(int id, string naziv)
+        {
+            Id = id;
+            Naziv = naziv;
+        }
+
+        public override string? ToString()
+        {
+            return $"[Device {Id}] = {Naziv}\n";
+        }
+    }
+}
---
+++ Domain/Models/Merenje.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Merenje
+    {
+        public int Id { get; set; } = 0;
+
+        public TipMerenja Tip {  get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public int Value { get; set; } = 0;
+
+        public Merenje() { }
+
+        public Merenje(int id, TipMerenja tip, DateTime timestamp, int value)
+        {
+            Id = id;
+            Tip = tip;
+            Timestamp = timestamp;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Timestamp} / {Tip} Merenje {Id}] = {Value}\n";
+        }
+    }
+}
