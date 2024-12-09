eb910ef43c3445c32519e71681ab947cb88b3034
Katarina16004 <veljkovickatarina56@gmail.com>
2024-12-09 11:19:17 +0100

    upis u log fajl

---
+++ Services/FileLoggerServices/FileLoggerService.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.FileLoggerServices
+{
+    public class FileLoggerService : ILoggerService
+    {
+        private string _putanja;
+
+        public FileLoggerService(string putanja="log.txt")
+        {
+            _putanja = putanja;
+        }
+
+        public bool Log(string poruka)
+        {
+            using StreamWriter sw = new(_putanja, append: true);
+            sw.Write($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture)}]: {poruka}\n");
+            return true;
+        }
+    }
+}
