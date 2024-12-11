0a1b88a543b8c20d39075157431005d6e2de8163
Katarina16004 <veljkovickatarina56@gmail.com>
2024-12-11 14:31:48 +0100

    slanje merenja

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,4 +1,7 @@
-﻿using Services.ProxySaveServices;
+﻿using Domain.Services;
+using Services.DeviceSendMerenjeServices;
+using Services.ProxySaveServices;
+using Services.ServerSaveDataServices;
 
 namespace Application
 {
@@ -7,6 +10,10 @@ namespace Application
         static void Main(string[] args)
         {
             Task.Run(() => ProxyInvalidateDataService.CheckAndUpdate());
+
+            ISaveDataService serverSaveDataService = new ServerSaveDataService();
+            var deviceSendMerenjeService = new DeviceSendMerenjeService(serverSaveDataService);
+            Task.Run(()=>deviceSendMerenjeService.PosaljiNovoMerenje());
             Console.ReadLine();
         }
     }
---
+++ Services/DeviceSendMerenjeServices/DeviceSendMerenjeService.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using Domain.Repositories.DeviceRepositories;
+using Domain.Repositories.MerenjaRepositories;
+using Domain.Repositories.ProxyDataRepositories;
+using Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DeviceSendMerenjeServices
+{
+    public class DeviceSendMerenjeService: IDeviceSendMerenjeService
+    {
+        static IDeviceRepository deviceRepository= new DeviceRepository();
+        static IMerenjaRepository merenjaRepository = new MerenjaRepository();
+        ISaveDataService serverSaveDataService;
+        public DeviceSendMerenjeService(ISaveDataService serverSaveDataService)
+        {
+            this.serverSaveDataService = serverSaveDataService;
+
+        }
+        public async Task PosaljiNovoMerenje()
+        {
+            while(true)
+            {
+                int brUredjaja= deviceRepository.SviUredjaji().Count();
+                int randomUredjajIndex = new Random().Next(0, brUredjaja);
+                int deviceId = deviceRepository.SviUredjaji().ToList()[randomUredjajIndex].Id;
+                int idMerenja= merenjaRepository.SvaMerenja().Count()+1;
+                TipMerenja tip = (TipMerenja)new Random().Next(0, Enum.GetValues(typeof(TipMerenja)).Length);
+                Merenje merenje = new Merenje(idMerenja, tip, DateTime.Now, new Random().Next(50,500), deviceId);
+
+                serverSaveDataService.SaveMerenje(merenje);
+                await Task.Delay(new Random().Next(5000,10000));
+            }
+        }
+    }
+}
