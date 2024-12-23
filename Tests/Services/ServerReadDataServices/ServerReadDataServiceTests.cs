ccb204ccc4c003b44d2ff9486801d6b9d6b2e560
Katarina16004 <veljkovickatarina56@gmail.com>
2024-12-23 15:10:30 +0100

    testovi za serverService

---
+++ Tests/Services/ServerReadDataServices/ServerReadDataServiceTests.cs
@@ -0,0 +1,144 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using Domain.Repositories.DeviceRepositories;
+using Domain.Repositories.MerenjaRepositories;
+using Moq;
+using Services.ServerReadDataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Services.ServerReadDataServices
+{
+    [TestFixture]
+    public class ServerReadDataServiceTests
+    {
+        Mock<IMerenjaRepository> _merenjaRepo;
+        Mock<IDeviceRepository> _deviceRepo;
+        ServerReadDataService _serverReadService;
+        public ServerReadDataServiceTests()
+        {
+            _merenjaRepo=new Mock<IMerenjaRepository>();
+            _deviceRepo=new Mock<IDeviceRepository>();
+            _serverReadService = new ServerReadDataService(_merenjaRepo.Object, _deviceRepo.Object);
+        }
+        [SetUp]
+        public void Setup()
+        {
+            _merenjaRepo = new Mock<IMerenjaRepository>();
+            _deviceRepo = new Mock<IDeviceRepository>();
+            _serverReadService = new ServerReadDataService(_merenjaRepo.Object, _deviceRepo.Object);
+        }
+
+        [Test]
+        [TestCase(TipMerenja.DIGITALNO)]
+        [TestCase(TipMerenja.ANALOGNO)]
+        public void ProcitajMerenjaPoTipu_VracaMerenja(TipMerenja tip)
+        {
+            var merenja = new List<Merenje>
+            {
+                new Merenje(1, TipMerenja.DIGITALNO, DateTime.Now, 100, 1),
+                new Merenje(2, TipMerenja.ANALOGNO, DateTime.Now, 200, 2),
+                new Merenje(3, TipMerenja.DIGITALNO, DateTime.Now, 300, 1)
+            };
+
+            _merenjaRepo.Setup(repo => repo.SvaMerenja()).Returns(merenja);
+            var vraceno = _serverReadService.ProcitajMerenjaPoTipu(tip);
+
+            Assert.IsNotNull(vraceno);
+            foreach (var merenje in vraceno)
+            {
+                Assert.AreEqual(tip, merenje.Tip);
+            }
+        }
+        [Test]
+        [TestCase(TipMerenja.DIGITALNO)]
+        [TestCase(TipMerenja.ANALOGNO)]
+        public void ProcitajMerenjaPoTipu_VracaPraznuListu(TipMerenja tip)
+        {
+            _merenjaRepo.Setup(repo => repo.SvaMerenja()).Returns(new List<Merenje>());
+
+            var vraceno = _serverReadService.ProcitajMerenjaPoTipu(tip);
+
+            Assert.IsNotNull(vraceno);
+            Assert.AreEqual(0, vraceno.Count());
+        }
+
+        [Test]
+        public void ProcitajNajnovijeMerenjePoDeviceId_VracaNajnovijeMerenje()
+        {
+            var merenja = new List<Merenje>
+            {
+                new Merenje(1, TipMerenja.DIGITALNO, DateTime.Parse("19.12.2024 13:32:35"), 25, 1),
+                new Merenje(2, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:37"), 60, 2),
+                new Merenje(3, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:40"), 1013, 1)//najnovije 
+            };
+
+            _merenjaRepo.Setup(repo => repo.SvaMerenja()).Returns(merenja);
+
+            var najnovijeMerenje = _serverReadService.ProcitajNajnovijeMerenjePoDeviceId(1);
+
+            Assert.IsNotNull(najnovijeMerenje);
+            Assert.AreEqual(3, najnovijeMerenje.Id);
+        }
+        [Test]
+        public void ProcitajNajnovijeMerenjeZaSvakiDevice_VracaMerenja()
+        {
+            var merenja = new List<Merenje>
+            {
+                new Merenje(1, TipMerenja.DIGITALNO, DateTime.Parse("19.12.2024 13:32:35"), 25, 1),
+                new Merenje(2, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:37"), 60, 2),
+                new Merenje(3, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:40"), 1013, 1)
+            };
+            var uredjaji = new List<Device>
+            {
+                new Device(1, "Uredjaj1"),
+                new Device(2, "Uredjaj2")
+            };
+
+            _merenjaRepo.Setup(repo => repo.SvaMerenja()).Returns(merenja);
+            _deviceRepo.Setup(repo => repo.SviUredjaji()).Returns(uredjaji);
+
+            var najnovijaMerenja = _serverReadService.ProcitajNajnovijeMerenjeZaSvakiDevice();
+
+            Assert.IsNotNull(najnovijaMerenja);
+            Assert.AreEqual(2, najnovijaMerenja.Count());
+            Assert.AreEqual(3, najnovijaMerenja.First(m => m.DeviceId == 1).Id);
+            Assert.AreEqual(2, najnovijaMerenja.First(m => m.DeviceId == 2).Id);
+        }
+        [Test]
+        public void ProcitajSvaMerenjaPoDeviceId_VracaSvaMerenjaZaDevice()
+        {
+            var merenja = new List<Merenje>
+            {
+                new Merenje(1, TipMerenja.DIGITALNO, DateTime.Parse("19.12.2024 13:32:35"), 25, 1),
+                new Merenje(2, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:37"), 60, 1),
+                new Merenje(3, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:40"), 1013, 1)
+            };
+
+            _merenjaRepo.Setup(repo => repo.SvaMerenja()).Returns(merenja);
+
+            var svaMerenja = _serverReadService.ProcitajSvaMerenjaPoDeviceId(1);
+            Assert.IsNotNull(svaMerenja);
+            Assert.AreEqual(3, svaMerenja.Count());
+            Assert.IsTrue(svaMerenja.All(m => m.DeviceId == 1));
+        }
+        [Test]
+        public void ProcitajSvaMerenjaPoDeviceId_NemaMerenjaZaDevice()
+        {
+            var merenja = new List<Merenje>
+            {
+                new Merenje(1, TipMerenja.DIGITALNO, DateTime.Parse("19.12.2024 13:32:35"), 25, 1),
+                new Merenje(2, TipMerenja.ANALOGNO, DateTime.Parse("19.12.2024 13:32:37"), 60, 2)
+            };
+
+            _merenjaRepo.Setup(repo => repo.SvaMerenja()).Returns(merenja);
+            var svaMerenja = _serverReadService.ProcitajSvaMerenjaPoDeviceId(3);
+
+            Assert.IsNotNull(svaMerenja);
+            Assert.AreEqual(0, svaMerenja.Count());
+        }
+    }
+}
---
+++ Tests/Services/ServerSaveDataServices/ServerSaveDataServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using Domain.Services;
+using Domain.Enums;
+using Domain.Models;
+using Services.ServerSaveDataServices;
+
+namespace Tests.Services.ServerSaveDataServices
+{
+    [TestFixture]
+    public class ServerSaveDataServiceTests
+    {
+        Mock<ISaveDataService> _serverSaveService;
+        public ServerSaveDataServiceTests() => _serverSaveService = new Mock<ISaveDataService>();
+        [SetUp]
+        public void Setup()
+        {
+            _serverSaveService = new Mock<ISaveDataService>();
+        }
+        [Test]
+        [TestCase(1, TipMerenja.DIGITALNO, "19.12.2024 13:32:35", 25, 1)]
+        [TestCase(2, TipMerenja.ANALOGNO, "19.12.2024 13:32:37", 60, 2)]
+        [TestCase(3, TipMerenja.ANALOGNO, "19.12.2024 13:32:34", 1013, 2)]
+        public void SaveMerenjeSaIspravnimPodacima_VracaTrue(int id, TipMerenja tip, string timestamp, int value, int deviceId)
+        {
+            DateTime date = DateTime.Parse(timestamp);
+            Merenje merenje = new(id, tip, date, value, deviceId);
+            _serverSaveService.Setup(x => x.SaveMerenje(merenje)).Returns(true);
+
+            bool uspesnoSaveMerenje = _serverSaveService.Object.SaveMerenje(merenje);
+            Assert.That(uspesnoSaveMerenje, Is.True);
+        }
+
+        [Test]
+        [TestCase(1, TipMerenja.DIGITALNO, "19.12.202413:32:35", 25, 1)]
+        [TestCase(2, TipMerenja.ANALOGNO, "19.12.202413:32:37", 60, 2)]
+        [TestCase(3, TipMerenja.ANALOGNO, "19.12.202413:32:34", 1013, 1)]
+        public void SaveMerenjeSaNeispravnimPodacima_VracaFalse(int id, TipMerenja tip, string timestamp, int value, int deviceId)
+        {
+            bool uspeh=DateTime.TryParse(timestamp,out DateTime date);
+            Merenje merenje = new(id, tip, date, value, deviceId);
+            if(!uspeh)
+                _serverSaveService.Setup(x => x.SaveMerenje(merenje)).Returns(false);
+
+            bool uspesnoSaveMerenje = _serverSaveService.Object.SaveMerenje(merenje);
+            Assert.That(uspesnoSaveMerenje, Is.False);
+        }
+    }
+}
