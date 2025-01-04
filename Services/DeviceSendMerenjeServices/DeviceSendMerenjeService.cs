3a05d7c6af0eb28816791f95ea2bac6e9003c08d
Katarina16004 <veljkovickatarina56@gmail.com>
2025-01-04 16:10:37 +0100

    testiranje slanja merenja

--- Services/DeviceSendMerenjeServices/DeviceSendMerenjeService.cs
+++ Services/DeviceSendMerenjeServices/DeviceSendMerenjeService.cs
@@ -2,13 +2,7 @@
 using Domain.Models;
 using Domain.Repositories.DeviceRepositories;
 using Domain.Repositories.MerenjaRepositories;
-using Domain.Repositories.ProxyDataRepositories;
 using Domain.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.DeviceSendMerenjeServices
 {
@@ -18,6 +12,7 @@ namespace Services.DeviceSendMerenjeServices
         static IMerenjaRepository merenjaRepository = new MerenjaRepository();
         ISaveDataService serverSaveDataService;
         ILoggerService loggerService;
+        public bool pozvano { get; private set; } = false;
         public DeviceSendMerenjeService(ISaveDataService serverSaveDataService, ILoggerService loggerService)
         {
             this.serverSaveDataService = serverSaveDataService;
@@ -25,20 +20,25 @@ namespace Services.DeviceSendMerenjeServices
         }
         public async Task PosaljiNovoMerenje()
         {
+            pozvano = true;
             while(true)
             {
-                int brUredjaja= deviceRepository.SviUredjaji().Count();
-                int randomUredjajIndex = new Random().Next(0, brUredjaja);
-                int deviceId = deviceRepository.SviUredjaji().ToList()[randomUredjajIndex].Id;
-                int idMerenja= merenjaRepository.SvaMerenja().Count()+1;
-                TipMerenja tip = (TipMerenja)new Random().Next(0, Enum.GetValues(typeof(TipMerenja)).Length);
-                Merenje merenje = new Merenje(idMerenja, tip, DateTime.Now, new Random().Next(50,500), deviceId);
-
-                loggerService.Log("Izmereno novo merenje...");
-
-                serverSaveDataService.SaveMerenje(merenje);
+                GenerisiMerenje();
                 await Task.Delay(new Random().Next(2000,7000));
             }
+        }
+        public void GenerisiMerenje()
+        {
+            int brUredjaja = deviceRepository.SviUredjaji().Count();
+            int randomUredjajIndex = new Random().Next(0, brUredjaja);
+            int deviceId = deviceRepository.SviUredjaji().ToList()[randomUredjajIndex].Id;
+            int idMerenja = merenjaRepository.SvaMerenja().Count() + 1;
+            TipMerenja tip = (TipMerenja)new Random().Next(0, Enum.GetValues(typeof(TipMerenja)).Length);
+            Merenje merenje = new Merenje(idMerenja, tip, DateTime.Now, new Random().Next(50, 500), deviceId);
+
+            loggerService.Log("Izmereno novo merenje...");
+
+            serverSaveDataService.SaveMerenje(merenje);
         }
     }
 }
---
+++ Tests/Services/DeviceSendMerenjeServices/DeviceSendMerenjeServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using Domain.Repositories.DeviceRepositories;
+using Domain.Repositories.MerenjaRepositories;
+using Domain.Services;
+using Moq;
+using Services.DeviceSendMerenjeServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Services.DeviceSendMerenjeServices
+{
+    [TestFixture]
+    public class DeviceSendMerenjeServiceTests
+    {
+        Mock<IDeviceRepository> _deviceRepo;
+        Mock<IMerenjaRepository> _merenjaRepo;
+        Mock<ILoggerService> _loggerService;
+        Mock<ISaveDataService> _saveDataService;
+        DeviceSendMerenjeService _sendMerenjeService;
+
+        public DeviceSendMerenjeServiceTests()
+        {
+            _deviceRepo = new Mock<IDeviceRepository>();
+            _merenjaRepo = new Mock<IMerenjaRepository>();
+            _loggerService = new Mock<ILoggerService>();
+            _saveDataService = new Mock<ISaveDataService>();
+
+            _sendMerenjeService = new DeviceSendMerenjeService(_saveDataService.Object, _loggerService.Object);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _deviceRepo = new Mock<IDeviceRepository>();
+            _merenjaRepo = new Mock<IMerenjaRepository>();
+            _loggerService = new Mock<ILoggerService>();
+            _saveDataService = new Mock<ISaveDataService>();
+
+            _sendMerenjeService = new DeviceSendMerenjeService(_saveDataService.Object, _loggerService.Object);
+        }
+        [Test]
+        public void GenerisiMerenje_GeneriseILoguje()
+        {
+            var uredjaji = new List<Device>
+            {
+                new Device (1,"Uredjaj1")
+            };
+
+            _deviceRepo.Setup(repo => repo.SviUredjaji()).Returns(uredjaji);
+            _sendMerenjeService.GenerisiMerenje();
+
+            _loggerService.Verify(logger => logger.Log(It.IsAny<string>()), Times.AtLeastOnce());
+            _saveDataService.Verify(save => save.SaveMerenje(It.IsAny<Merenje>()), Times.AtLeastOnce());
+        }
+        [Test]
+        public void GenerisiMerenje_IspravnostVrednostiMerenja()
+        {
+            var uredjaji = new List<Device>
+            { 
+                new Device (1,"Uredjaj1")
+            };
+
+            _deviceRepo.Setup(repo => repo.SviUredjaji()).Returns(uredjaji);
+            _sendMerenjeService.GenerisiMerenje();
+
+            _saveDataService.Verify(save => save.SaveMerenje(It.Is<Merenje>(m =>m.DeviceId == 1 || m.DeviceId == 2 
+                && m.Value >= 50 && m.Value <= 500 && Enum.IsDefined(typeof(TipMerenja), m.Tip))), Times.AtLeastOnce());
+        }
+        [Test]
+        public async Task PosaljiNovoMerenje_PozivMetode()
+        {
+            var task = _sendMerenjeService.PosaljiNovoMerenje();
+            await Task.Delay(500);
+
+            Assert.IsTrue(_sendMerenjeService.pozvano);
+        }
+    }
+}
